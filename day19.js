a7710c8737cf1472d0061c84e10e8c69ba271bde
katie hoffman <kathryn.hoffman@detroitlabs.com>
2017-03-23 14:09:37 -0400

    Adding day19.js file

---
+++ day19.js
@@ -0,0 +1,11 @@
+/*
+
+- Create an app that simulates the time-honored game "Rock Paper Scissors Lizard Spock"
+
+- Your game should allow the user to select a hand shape and play against the computer (which will select a random hand shape)
+
+- The game should notify the user of the outcome, and should offer the user the option to play again
+
+- The game should tell the user how they won or lost (e.g., display the message "Paper disproves Spock!")
+
+*/
